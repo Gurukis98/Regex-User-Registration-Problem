c8731665efd51c38d718f21433267fab0dee8f95
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-26 00:02:17 +0530

    [Guru]Add:Enter a Valid Firstname

--- Regex UC-1/Regex UC-1/Program.cs
+++ Regex UC-1/Regex UC-1/Program.cs
@@ -16,7 +16,7 @@ namespace Regex_UC_1
             }
             else
             {
-                Console.WriteLine("Firstname is Not Valid");
+                Console.WriteLine("Firstname is Not Valid ");
             }
         }
     }
