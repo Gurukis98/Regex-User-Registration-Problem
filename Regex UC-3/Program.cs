aae2304743ac49610b2304bb598dc598bb63e4ec
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-28 20:55:37 +0530

    [Guru]Add:Enter a Valid Email

--- Regex UC-3/Program.cs
+++ Regex UC-3/Program.cs
@@ -8,9 +8,9 @@ namespace Regex_UC_3
     {
        public static void Main(string[] args)
         {
-            Console.WriteLine("Enter Your EMail Like (Eg:abc.@bl.co)");
+            Console.WriteLine("Enter Your EMail Like (Eg:abc01@gmail.com)");
             string email = Console.ReadLine();
-            if (Regex.Match(email, "^[0-9a-z]{1, }[.][@][a-z]{1, }[.][a-z]{1, }$").Success)
+            if (Regex.Match(email, "^[0-9a-z]{1,}[@][a-z]{5}[.][a-z]{3}$").Success)
             {
                 Console.WriteLine("EMail is Valid");
             }
