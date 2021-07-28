4a0b224b0c8c676fe08e3d3f1d6068ee8eb7b249
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-28 21:31:53 +0530

    [Guru]Add:Enter Your Password Use Atleast 1 Numeric Character

--- Regex UC-7/Program.cs
+++ Regex UC-7/Program.cs
@@ -9,7 +9,7 @@ namespace Regex_UC_7
         {
             Console.WriteLine("Enter Your Password Should Have Minimum 8 Characters, Atleast 1 Uppercase and Atleast 1 Numeric Number");
             string rule3 = Console.ReadLine();
-            if (Regex.Match(rule3, "^[A-Z0-9a-z]{6,}[A-Z][0-9]$").Success)
+            if (Regex.Match(rule3, "^[A-Z0-9a-z]{8,}$").Success)
             {
                 Console.WriteLine("Password is Valid");
             }
