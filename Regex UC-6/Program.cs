a7f0a98b1045eacf93ab967d1159360d656c90e9
Gurukis98 <84782507+Gurukis98@users.noreply.github.com>
2021-07-28 21:32:44 +0530

    [Guru]Add:Enter Your Password Use Atleast 1 Upper Case

--- Regex UC-6/Program.cs
+++ Regex UC-6/Program.cs
@@ -9,7 +9,7 @@ namespace Regex_UC_6
         {
             Console.WriteLine("Enter Your Password Should Have Minimum 8 Characters and 1 Uppercase");
             string rule2 = Console.ReadLine();
-            if (Regex.Match(rule2, "^[A-Za-z]{7,}[A-Z]$").Success)
+            if (Regex.Match(rule2, "^[A-Za-z]{8,}$").Success)
             {
                 Console.WriteLine("Password is Valid");
             }
